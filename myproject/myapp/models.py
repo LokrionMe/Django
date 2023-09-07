ba1f6ca2e2d0c17b27e957e59d7f07cb25c86124
Lokrion <m.kostian@example.com>
2023-09-07 14:06:07 +0700

    done hw2

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    number = models.CharField(max_length=30)
+    addres = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    count = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    
